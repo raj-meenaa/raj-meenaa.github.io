562278b5db3857766bd50465c36abe2ecc050a47
Raj Meena <heyrajmeena@gmail.com>
2025-05-21 17:19:56 +0530

    Update Experience

--- src/constants/experience.js
+++ src/constants/experience.js
@@ -7,10 +7,25 @@ export const EXPERIENCES = [
     // technologies: ["Node.js", "React.js", "Express.js", "mongoDB"],
   },
   {
+    year: "Nov 2024 - Feb 2025",
+    role: "Web Developement Intern",
+    company: "Indian Institute Of Technology, Guwahati",
+    description:`Collaborated in a 3-member team to design and develop an interactive web-based software platform that accepts machining parameters, simulates chatter behavior, and visualizes stability lobes for various machining processes. Developed a dynamic frontend using Vite and React, integrated with a Python backend for computational simulations of Milling SDOF, Turning SDOF, and Turning 2DOF models. Integrated Supabase for secure user authentication and data management, enabling users to save and retrieve their simulation results seamlessly.`,
+    // technologies: ["Node.js", "React.js", "Express.js", "mongoDB"],
+  },
+  {
+    year: "Dec 2024 - Feb 2025",
+    role: "Full-Stack Developer | Website Development",
+    company: "Hirrent",
+    description:`Independently developed and deployed the official website for Hirrent using the MERN stack. Focused on responsive UI/UX, API integrations, and state management using Zustand. Handled deployment on AWS EC2 and implemented robust frontend components for the User, Seller, and Admin panels.`,
+    // technologies: ["Node.js", "React.js", "Express.js", "mongoDB"],
+  },
+
+  {
     year: "Jan 2023 - Dec 2023",
     role: "Development Team Coordinator",
     company: "Hirrent",
-    description:`Led a team of 5 developers, ensuring seamless collaboration and timely delivery of high-quality software solutions, improved project delivery time by 20% by implementing Agile methodologies, and resolved critical compatibility issues through effective problem-solving and team collaboration.`,
+    description:`Led a team of 5 developers, ensuring seamless collaboration and timely delivery of high-quality software solutions. Improved project delivery time by 20% through Agile methodology implementation and resolved critical compatibility issues via effective problem-solving and team collaboration.`,
     // technologies: ["Node.js", "React.js", "Express.js", "mongoDB"],
   },
 ];
